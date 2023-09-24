572179683a48789666101d4389a9b0b63bf12b75
Samrat <samratapr40@gmail.com>
2023-09-24 17:44:59 +0530

    subscribe module

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Button from '../components/Button'
 
 function Subscribe() {
   return (
-    <div>Subscribe</div>
+    <section
+    id='contact-us'
+    className='max-container flex justify-between items-center max-lg:flex-col gap-10'>
+      <h3 className='text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold'>
+        Sign Up for <span className='text-coral-red'>Updates</span> & News Letter
+      </h3>
+      <div
+      className='lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full'>
+        <input 
+          type="text" 
+          name="email"
+          placeholder='samratapr40@gmail.com'
+          className='input'  
+        />
+        <div
+        className='flex max-sm:justify-end items-center max-sm:w-full'
+        >
+          <Button label="Sign Up" fullWidth/>
+        </div>
+      </div>
+    </section>
   )
 }
 
